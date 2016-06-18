45a03cbf9e1f90674570e6a334e000f8864384dc
yujinwatabe <15c1020043wy@edu.tech.ac.jp>
2016-06-18 14:33:01 +0900

    1606/18スクリプトの追加

---
+++ Assets/Script/setumei.cs
@@ -0,0 +1,52 @@
+﻿ using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class setumei : MonoBehaviour {
+    public Image setumeiimage;
+    public GameObject text;
+    public string setuneutext;
+    public string setuneutext2;
+    public float readtyme = 2f;
+    public bool a = true;
+    float i = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (a)
+            {
+                a = false;
+                StartCoroutine("setumeikaisi");
+            }
+        }
+    }
+    IEnumerator setumeikaisi()
+    {
+        i = 0;
+        setumeiimage.enabled = true;
+        text.SetActiveRecursively(true);
+        text.GetComponent<Text>().text= setuneutext.ToString();
+        yield return new WaitForSeconds(readtyme);
+        text.SetActiveRecursively(false);
+        setumeiimage.enabled = false;
+        if (setuneutext2 != null)
+        {
+            setumeiimage.enabled = true;
+            text.SetActiveRecursively(true);
+            text.GetComponent<Text>().text = setuneutext2.ToString();
+            yield return new WaitForSeconds(readtyme);
+            text.SetActiveRecursively(false);
+            setumeiimage.enabled = false;
+        }
+    }
+}
---
+++ Assets/Script/start.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class start : MonoBehaviour
+{
+    public GameObject shatta;
+    public Text buttontext;
+    public Text title;
+    public Sprite snaarashi;
+    private Color originalColor;
+    float a, b;
+    float time;
+    bool Fadeout = true;
+    float z;
+    public void Buttonpush()
+    {
+        if (Fadeout)
+        {
+            Screen.autorotateToLandscapeRight = false;
+            Screen.autorotateToLandscapeLeft = false;
+            StartCoroutine("FadeOut");
+            Fadeout = false;
+        }
+
+
+    }
+    IEnumerator FadeOut()
+    {
+        AsyncOperation async = Application.LoadLevelAsync("stage1");//シーンのロード
+        async.allowSceneActivation = false;//だがまだシーンの移動はしない
+        
+        iTween.ScaleTo(gameObject, iTween.Hash("x", Screen.width, "y", Screen.height, "time", 40.0f));
+        iTween.MoveTo(gameObject, iTween.Hash("x", Screen.width / 2, "y", Screen.height / 2, "time", 40.0f));
+        
+        
+        yield return new WaitForSeconds(1f);
+        Image img = this.GetComponent<Image>();
+        img.sprite = snaarashi;
+        for (; ; )
+        {
+            if(z == 180){
+                z = 0;
+            }
+            else
+            {
+                z = 180;
+            }
+            
+            gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, z));
+            yield return new WaitForSeconds(0.05f);
+        }
+        yield return new WaitForSeconds(3.0f);
+        async.allowSceneActivation = true;
+    }
+}
+
